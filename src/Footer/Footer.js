fe1b308482d6225b63bea28d1b2ca94e67f4c69f
potman-tech <blinok1896@gmail.com>
2022-01-23 15:51:23 +0400

    fix

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,46 +1,58 @@
-import React, { useState } from 'react'
-import './FooterStyle/FooterStyle.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { push, pushme } from '../action/action'
-import manI from '../img/manI.jpg'
-import { IamMan } from '../dataUser'
+import React, { useState } from "react";
+import "./FooterStyle/FooterStyle.css";
+import { useSelector, useDispatch } from "react-redux";
+import { push, pushme } from "../action/action";
+import manI from "../img/manI.jpg";
+import { IamMan } from "../dataUser";
 
-export default function Footer (  ){ 
-   // console.log(IamMan.text)
-   
-    const [inptValue, setInptValue] = useState('')
-    
+export default function Footer() {
  
 
-   
-    
-    // console.log(IamMan)
-    const dispatch = useDispatch()
-    return <div className='Footer'> 
-    <button onClick={()=> {
-        dispatch(push())
-    }}>New Message from friends</button>
-    <div className="Footer__input">
-        <input type="text" id='inpt' onChange={e=>setInptValue(e.target.value)
-        }/>
-        <button onClick={()=>{
-            
-           if(inptValue.length >= 1)
-           {
-            document.querySelector('#errs').innerHTML = ''
-               IamMan.mytext = inptValue
-            return dispatch(pushme())
-           }
-           else {
-               document.querySelector('#errs').innerHTML = 'write something to your friends'
-               
-           }
-           
-        }}>send</button>
-        {inptValue.length < 1 ? <img src={manI} alt="i'm" /> : <div className='myIcon'><img src={manI} alt="i'm" />
-        <div className='whiteBl'></div></div> }
-        <p id='errs' ></p>
-    </div>
-    
+  const [inptValue, setInptValue] = useState("");
 
- </div>}
+  
+  const dispatch = useDispatch();
+  return (
+    <div className="Footer">
+      <button
+        onClick={() => {
+          dispatch(push());
+        }}
+      >
+        New Message from friends
+      </button>
+      <form className="Footer__input">
+        <input
+          type="text"
+          id="inpt"
+          onChange={(e) => setInptValue(e.target.value)}
+        />
+        <button
+          type="reset"
+          onClick={() => {
+            if (inptValue.length >= 1) {
+              document.querySelector("#errs").innerHTML = "";
+              IamMan.mytext = inptValue;
+              setInptValue("");
+              return dispatch(pushme());
+            } else {
+              document.querySelector("#errs").innerHTML =
+                "write something to your friends";
+            }
+          }}
+        >
+          send
+        </button>
+        {inptValue.length < 1 ? (
+          <img src={manI} alt="i'm" />
+        ) : (
+          <div className="myIcon">
+            <img src={manI} alt="i'm" />
+            <div className="whiteBl"></div>
+          </div>
+        )}
+        <p id="errs"></p>
+      </form>
+    </div>
+  );
+}
